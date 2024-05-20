e2539e230f2d58de41ce0ccff983e82b5a09644e
mabyoungg <mabyoungg@gmail.com>
2024-05-20 16:43:29 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/example/springbootsveltekitrestback/SpringBootSveltekitRestBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootsveltekitrestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSveltekitRestBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSveltekitRestBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springbootsveltekitrestback/SpringBootSveltekitRestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootsveltekitrestback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSveltekitRestBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
