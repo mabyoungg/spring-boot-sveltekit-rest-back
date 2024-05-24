fcd4fbef07e9d2247550b5dd436725f46200f0d5
mabyoungg <mabyoungg@gmail.com>
2024-05-24 22:46:36 +0900

    feat: GlobalExceptionHandler 추가

---
+++ src/main/java/org/example/springbootsveltekitrestback/global/exceptionsHandlers/GlobalExceptionHandler.java
@@ -0,0 +1,62 @@
+package org.example.springbootsveltekitrestback.global.exceptionsHandlers;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springbootsveltekitrestback.global.exceptions.GlobalException;
+import org.example.springbootsveltekitrestback.global.rq.Rq;
+import org.example.springbootsveltekitrestback.global.rsData.RsData;
+import org.example.springbootsveltekitrestback.standard.base.Empty;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+    private final Rq rq;
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex) {
+        // if (rq.isApi()) throw ex;
+
+        return handleApiException(ex);
+    }
+    
+    private ResponseEntity<Object> handleApiException(Exception ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("resultCode", "500-1");
+        body.put("statusCode", 500);
+        body.put("msg", ex.getLocalizedMessage());
+
+        LinkedHashMap<String, Object> data = new LinkedHashMap<>();
+        body.put("data", data);
+
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        data.put("trace", sw.toString().replace("\t", "    ").split("\\r\\n"));
+
+        String path = rq.getCurrentUrlPath();
+        data.put("path", path);
+
+        body.put("success", false);
+        body.put("fail", true);
+
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(GlobalException.class)
+    @ResponseStatus // error 내용의 스키마를 타입스크립트
+    public ResponseEntity<RsData<Empty>> handle(GlobalException ex) {
+        HttpStatus status = HttpStatus.valueOf(ex.getRsData().getStatusCode());
+        rq.setStatusCode(ex.getRsData().getStatusCode());
+
+        return new ResponseEntity<>(ex.getRsData(), status);
+    }
+}
