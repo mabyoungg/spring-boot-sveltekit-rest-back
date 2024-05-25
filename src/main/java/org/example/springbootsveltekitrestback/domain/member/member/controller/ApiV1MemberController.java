8cdb1bc4eda95743cf704f1b74b5cb8ff80302bb
mabyoungg <mabyoungg@gmail.com>
2024-05-25 19:43:37 +0900

    feat: 로그인 처리

---
+++ src/main/java/org/example/springbootsveltekitrestback/domain/member/member/controller/ApiV1MemberController.java
@@ -0,0 +1,45 @@
+package org.example.springbootsveltekitrestback.domain.member.member.controller;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.example.springbootsveltekitrestback.domain.member.member.dto.MemberDto;
+import org.example.springbootsveltekitrestback.domain.member.member.service.MemberService;
+import org.example.springbootsveltekitrestback.global.rq.Rq;
+import org.example.springbootsveltekitrestback.global.rsData.RsData;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+public class ApiV1MemberController {
+    private final MemberService memberService;
+    private final Rq rq;
+
+    public record LoginRequestBody(@NotBlank String username, @NotBlank String password) {
+    }
+
+    public record LoginResponseBody(@NonNull MemberDto item) {
+    }
+
+    @PostMapping("/login")
+    public RsData<LoginResponseBody> login(@Valid @RequestBody LoginRequestBody body) {
+        RsData<MemberService.AuthAndMakeTokensResponseBody> authAndMakeTokensRs = memberService.authAndMakeTokens(
+                body.username,
+                body.password
+        );
+
+        rq.setCrossDomainCookie("refreshToken", authAndMakeTokensRs.getData().refreshToken());
+        rq.setCrossDomainCookie("accessToken", authAndMakeTokensRs.getData().accessToken());
+
+        return authAndMakeTokensRs.newDataOf(
+                new LoginResponseBody(
+                        new MemberDto(authAndMakeTokensRs.getData().member())
+                )
+        );
+    }
+}
