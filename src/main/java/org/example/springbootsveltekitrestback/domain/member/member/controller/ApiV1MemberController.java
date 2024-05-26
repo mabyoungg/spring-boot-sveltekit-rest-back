53845eee1df9efb19ad1f093da0b835c7e43cb42
mabyoungg <mabyoungg@gmail.com>
2024-05-26 18:37:09 +0900

    refactor: 로그인 유지

--- src/main/java/org/example/springbootsveltekitrestback/domain/member/member/controller/ApiV1MemberController.java
+++ src/main/java/org/example/springbootsveltekitrestback/domain/member/member/controller/ApiV1MemberController.java
@@ -7,11 +7,9 @@ import org.example.springbootsveltekitrestback.domain.member.member.dto.MemberDt
 import org.example.springbootsveltekitrestback.domain.member.member.service.MemberService;
 import org.example.springbootsveltekitrestback.global.rq.Rq;
 import org.example.springbootsveltekitrestback.global.rsData.RsData;
+import org.example.springbootsveltekitrestback.standard.base.Empty;
 import org.springframework.lang.NonNull;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/members")
@@ -41,5 +39,24 @@ public class ApiV1MemberController {
                         new MemberDto(authAndMakeTokensRs.getData().member())
                 )
         );
+    }
+
+    public record MeResponseBody(@NonNull MemberDto item) {
+    }
+
+    @GetMapping("/me")
+    public RsData<MeResponseBody> getMe() {
+        return RsData.of(
+                new MeResponseBody(
+                        new MemberDto(rq.getMember())
+                )
+        );
+    }
+
+    @PostMapping("/logout")
+    public RsData<Empty> logout() {
+        rq.setLogout();
+
+        return RsData.of("200", "로그아웃 성공");
     }
 }
--- src/main/java/org/example/springbootsveltekitrestback/global/security/ApiSecurityConfig.java
+++ src/main/java/org/example/springbootsveltekitrestback/global/security/ApiSecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     @Order(2)
     SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
@@ -24,7 +27,7 @@ public class ApiSecurityConfig {
                                 .requestMatchers("/api/*/members/login", "/api/*/members/logout")
                                 .permitAll()
                                 .anyRequest()
-                                .permitAll()
+                                .authenticated()
                 )
                 .csrf(
                         csrf -> csrf
@@ -35,7 +38,8 @@ public class ApiSecurityConfig {
                                 .sessionCreationPolicy(
                                         SessionCreationPolicy.STATELESS
                                 )
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
